57aed1bce9c05134ccbf685e708459b86f9e1d44
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-13 20:40:37 +0500

    topBottom

--- funcon-project/src/components/topBottom.jsx
+++ funcon-project/src/components/topBottom.jsx
@@ -1,12 +1,21 @@
-import tB from './imgs/image 6.png'
-
+import tB from "./imgs/image 6.png";
+import network from "./imgs/image 21.png";
+import wallet from "./imgs/image 20.png";
+import academy from "./imgs/image 19.png";
+import education from "./imgs/image 11.png";
 
 const TopBottom = () => {
   return (
-    <div className='bg-cover bg-center h-screen' style={{backgroundImage:`url(${tB})`}}>
-
+    <div
+      className="bg-cover bg-center h-screen"
+      style={{ backgroundImage: `url(${tB})` }}
+    >
+      <div className=" flex justify-center items-center gap-40 pt-20">
+        <div className=" grid grid-cols-1 gap-20"><div><img src={network} alt="" /></div><div className=" flex items-center gap-20"><img src={wallet} alt="" /><img src={academy} alt="" /></div></div>
+        <div><img src={education} alt="" /></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopBottom
+export default TopBottom;
