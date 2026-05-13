c93c38072377767faa21e9918f2f4803f9acc1e1
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-13 21:10:34 +0500

    middleBottom

--- funcon-project/src/components/middle.jsx
+++ funcon-project/src/components/middle.jsx
@@ -10,7 +10,7 @@ const Middle = () => {
   return (
     <div className=' relative'>
     <div style={{backgroundImage:`url(${middlefr})`}} className='bg-cover bg-center h-screen'><p className=' font-bold text-white text-5xl text-center pt-20'>Decentralize and Performance</p> <img src={f} alt=""  className=' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-150'/> <img src={side} alt="" className=' absolute w-80 top-60 left-40'/> <img src={pwa} alt="" className=' absolute w-80 top-85 right-30'/></div>
-    <div style={{backgroundImage:`url(${middlesc})`}} className='bg-cover bg-center h-screen'><img src={maining} alt=""  className='w-80 absolute top-210 left-45'/> <img src={nodes} alt=""  className='w-80 absolute top-240 right-60'/></div>
+    <div style={{backgroundImage:`url(${middlesc})`}} className='bg-cover bg-center h-screen'><img src={maining} alt=""  className='w-80 absolute top-210 left-45'/> <img src={nodes} alt=""  className='w-80 absolute top-240 right-60'/><p className=' font-bold text-white text-5xl  pt-150 w-170 pl-30'>Privacy and Anonymity Fact NASA</p></div>
     </div>
 
   )
