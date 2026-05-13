4e135f2c6887d9fe3ca0ff87cf9c77a1e17af5ad
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-13 20:42:02 +0500

    jsx

--- funcon-project/src/App.jsx
+++ funcon-project/src/App.jsx
@@ -1,11 +1,13 @@
 import Top from "./components/top";
 import TopBottom from "./components/topBottom";
+import Middle from "./components/middle";
 
 function App() {
   return (  
 <div>
 <Top/>
 <TopBottom/>
+<Middle/>
 </div>
 
   );
---
+++ funcon-project/src/components/middle.jsx
@@ -0,0 +1,9 @@
+
+
+const Middle = () => {
+  return (
+    <div>middle</div>
+  )
+}
+
+export default Middle
